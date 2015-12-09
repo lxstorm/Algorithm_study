5f09c5528fc40c61ebb29aa97756f28fcd820995
lxstorm <lxstormy@gmail.com>
2015-12-09 21:05:40 +0800

    Stack

---
+++ ch3/Stack.h
@@ -0,0 +1,15 @@
+#ifdef _Stack_h
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode Stack;
+
+int IsEmpty(Stack S);
+Stack CreateStack(void);
+void DisposeStack(Stack S);
+void MakeEmpty(Stack S);
+void Push(ElementType X, Stack S);
+ElemnetType Top(Stack S);
+void Pop(Stack S);
+
+#endif /*_Stack_h*/
