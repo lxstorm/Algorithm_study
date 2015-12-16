1a7469df971286a45d55d8286124283d97c7eec4
lxstorm <lxstormy@gmail.com>
2015-12-16 23:40:01 +0800

    BinarySearchTree

--- ch4/BinarySearchTree.c
+++ ch4/BinarySearchTree.c
@@ -45,3 +45,61 @@ Position FindMax(SearchTree T)
 			T = T->Right;
 	return T;
 }
+
+SearchTree Insert(ElementType X, SearchTree T)
+{
+	if (T == NULL)
+		/*Create and return a one-node tree*/
+		T =  malloc(sizeof(struct TreeNode));
+		if(T == NULL)
+			FatalError("Out of space!!!");
+		else
+		{
+		T->Element = X;
+		T->Left = NULL;
+		T->Right = NULL;
+		}
+	else
+	if(X < T->Element)
+		T->Left = Insert(X, T->Left);
+	else
+	if(X > T->Element)
+		T->Right = Insert(X, T->Right);
+	
+	return T;
+}
+
+SearchTree Delete(ElementType X, SearchTree)
+{
+	Position TmpCell;
+
+	if(T == NULL)
+		Error("Element not found");
+	else if (X < T->Left)
+		T->Left = Delete(X, T->Left);	/*Go left*/
+	else if(X > T->Right)
+		T->Right = Delete(X, T->Right);	/*Go right*/
+	else /*Found Element to be deleted*/
+	{
+		if(T->Left && T->Right)	 /*Two children*/
+		{
+		/*Replace with smallest in right subtree*/
+			TmpCell = FindMin(T->Right);
+			T->Element = TmpCell->Element;
+			T->Right = Delete(T->Element, T->Right);
+		}
+		else /*One or Zero children*/
+		{
+			TmpCell = T;
+			if(T->Left == NULL)
+				T = T->Left;
+			else if(T->Right = NULL)
+				T = T->Right;
+			free(TmpCell);
+		}
+
+		return T;
+	}
+
+
+}
