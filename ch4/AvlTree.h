e040cad58a3ab6ac04ffa47ac94196b27a25209f
lxstorm <lxstormy@gmail.com>
2015-12-17 19:34:28 +0800

    AvlTree

---
+++ ch4/AvlTree.h
@@ -0,0 +1,15 @@
+#ifdef _AvlTree_H
+
+struct AvlNode;
+typedef struct AvlNode *Position;
+typedef struct AvlNode *AvlTree;
+
+AvlTree Makeempty(AvlTree T);
+Position Find(ElementType X, AvlTree T);
+Position FindMin(AvlTree T);
+Position FIndMax(AvlTree T);
+AvlTree Insert(ElementType X, AvlTree T);
+AvlTree Delete(ElementType X, AvlTree T);
+ElementType Retrieve(Position P);
+
+#endif /*_AvlTree_H*/
